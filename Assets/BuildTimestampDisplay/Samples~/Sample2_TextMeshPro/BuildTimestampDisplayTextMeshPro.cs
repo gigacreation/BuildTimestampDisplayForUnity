6d81cc7e5169c281d57a688dcc54a62e589d48e9
Gigacee <gigacee@gigacreation.jp>
2020-10-05 23:05:02 +0900

    Add TextMesh Pro sample

---
+++ Assets/BuildTimestampDisplay/Samples~/Sample2_TextMeshPro/BuildTimestampDisplayTextMeshPro.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace BuildTimestampDisplay.Sample2
+{
+    [RequireComponent(typeof(TextMeshProUGUI)), AddComponentMenu("Gigacee/Build Timestamp Display TextMesh Pro")]
+    public class BuildTimestampDisplayTextMeshPro : MonoBehaviour
+    {
+        [SerializeField] BuildTimestamp buildTimestamp;
+
+        void Start()
+        {
+            GetComponent<TextMeshProUGUI>().SetText(buildTimestamp.timestamp);
+        }
+    }
+}
